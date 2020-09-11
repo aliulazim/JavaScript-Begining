682216bf1164a0d44a3138e533d55cacc3e9a5db
aliul azim <aliulazimbd@gmail.com>
2020-09-11 09:10:00 +0600

    JS Varible

---
+++ 01_varible/app.js
@@ -0,0 +1,82 @@
+var planet = "Earth";
+var nextPlanet = "Mars";
+var anotherPlanet = "Jupiter";
+console.log(planet, nextPlanet, anotherPlanet);
+
+
+// Varible value cheange
+let myNumber = 12;
+myNumber = 13;
+console.log(myNumber);
+
+myNumber = 14;
+console.log(myNumber);
+
+
+// console
+let myName = "NurAlom";
+let myAge = 7;
+console.log('My Name Is', myName, 'and My age is', myAge, 'Years');
+
+
+// const
+const pi = 3.21; 
+// pi = 4.256; // const value con not be cheange
+console.log(pi);
+
+
+
+
+/* 
+JavaScript Reserved Words
+
+    They cannot be used as variable identifiers.
+
+await
+break
+case
+catch
+class
+const
+continue
+debugger
+default
+delete
+do
+else
+enum
+export
+extends
+false
+finally
+for
+function
+if
+implements
+import
+in
+instanceof
+interface
+let
+new
+null
+package
+private
+protected
+public
+return
+super
+switch
+static
+this
+throw
+try
+True
+typeof
+var
+void
+while
+with
+yield
+
+*/
