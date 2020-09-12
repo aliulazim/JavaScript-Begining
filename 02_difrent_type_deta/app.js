932c0c11debf78826300753f668d8cfb2b0c6f7c
aliul azim <aliulazimbd@gmail.com>
2020-09-12 16:47:51 +0600

    Difrent Type of Deta

---
+++ 02_difrent_type_deta/app.js
@@ -0,0 +1,30 @@
+// একসাথে কয়েকটি ভ্যারিয়েবলে ডেটা অ্যাসাইন
+//  let a = 12;
+//  let b = 13;
+//  let c = 14;
+//  let d = 15;
+
+//  let a = 12, b = 13, c = 14, d = 15;
+//  console.log(a, b, c, d);
+
+
+//  let a = 2;
+//  let b = 2;
+//  let c = 2;
+//  let d = 4;
+
+//  let a = 2, b = 2, c = 2, d = 4;
+let a = b = c = 2;
+let d = 4;
+
+ console.log(a, b, c, d);
+
+
+ // Deta type
+  let word = "earth";
+  let pi = 3.14159;
+  let earthIsRound = true;
+
+  console.log(typeof word);
+  console.log(typeof pi);
+  console.log(typeof earthIsRound);
