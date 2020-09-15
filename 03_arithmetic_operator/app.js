e17794eef2476e259eb2635268d4c10463ac1291
aliul azim <aliulazimbd@gmail.com>
2020-09-15 10:58:04 +0600

    Math Arithmetic (Arithmetic Operators)

---
+++ 03_arithmetic_operator/app.js
@@ -0,0 +1,27 @@
+// Cost of Eggs - Calculator
+
+let eggPrice = 5;
+let numberOfEggs = 5;
+let totalCostOfEggs = eggPrice * numberOfEggs;
+console.log("Total cost of ", totalCostOfEggs, "Taka");
+
+// Area of a Circle Calculator = pi * r squared
+
+let radius = 7;
+const pi = 3.14159;
+let areaOfCircle = pi * (radius * radius);
+console.log("Area of the circle is ", areaOfCircle, " sq inch");
+
+
+// USD to BDT calculator
+
+let usdToBdt = 84.3;
+let bdt = 10000;
+let usd = bdt / usdToBdt;
+console.log("$ ", usd);
+
+
+// use %
+let n1 = 5;
+let r1 = 5 % 2;
+console.log("Vag sas ", r1);
