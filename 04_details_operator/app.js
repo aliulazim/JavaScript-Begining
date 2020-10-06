991ff37a78a97c1163390e0379404a01f99f0abe
aliul azim <aliulazimbd@gmail.com>
2020-10-06 09:40:53 +0600

    Advances Operator

---
+++ 04_details_operator/app.js
@@ -0,0 +1,40 @@
+/*
+let x = 4;
+x++; // x = x + 1
+x--; // x = x - 1
+console.log(x);
+*/
+   
+let x;
+let y = 5;
+
+/*
+// Jakhon ++ pare thake takhon age man pass kore dei and pare ++ man kaj kare.
+
+// x = y++;  // ai code ke onovabe likha jai
+x = y;
+y = y + 1;
+*/
+
+
+// Jakhon ++ age base thakhon prothome ++ man kaj kere
+
+// x = ++y;
+y = y + 1;
+x = y;
+
+
+
+console.log("X = ", x, "Y = ", y);
+
+
+
+// assignment operators
+
+let  x = 4;
+x += 4; // x = x + 4;
+// x -= 2; // x = x - 2
+// x *= 3; // x = x * 3
+// x /= 2; // x = x / 2
+x %= 2; // x = x % 2
+console.log(x); 
