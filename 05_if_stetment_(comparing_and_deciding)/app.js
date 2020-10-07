54bd36183c0db7434eafa9476ba7486591d8f2a2
aliul azim <aliulazimbd@gmail.com>
2020-10-07 21:34:55 +0600

    if_stetment_(comparing_and_deciding)

---
+++ 05_if_stetment_(comparing_and_deciding)/app.js
@@ -0,0 +1,35 @@
+let n = 7;
+
+if (n < 10) {
+    console.log(n);
+    console.log("This Number", n, "is smaller than 10");
+}
+else if (n < 20) {
+    console.log("This number", n, "is smaller than 20 ");
+}
+else{
+    console.log("This Number", n, "is greater than 10");
+};
+
+
+let x = 23;
+let y = 23;
+
+if(x > y){
+    console.log("x is greater than y");
+}else if (x === y){
+    console.log("x is equal to y");
+}else{
+    console.log("x is smaller than y");
+}
+
+
+let a = 10;
+
+if (8 == a){
+    console.log("a is equal to 8");
+}else{
+    console.log("a is not equal to 8");
+
+}
+console.log(a);
