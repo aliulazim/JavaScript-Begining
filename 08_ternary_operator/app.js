1300e19825a1c356d0443151d72afab41910b5a4
aliulazim <aliulazimbd@gmail.com>
2020-11-07 07:41:23 +0600

    Ternary Operator

--- 08_ternary_operator/app.js
+++ 08_ternary_operator/app.js
@@ -1,82 +1,15 @@
-var planet = "Earth";
-var nextPlanet = "Mars";
-var anotherPlanet = "Jupiter";
-console.log(planet, nextPlanet, anotherPlanet);
+let n = -11;
+let result;
 
 
-// Varible value cheange
-let myNumber = 12;
-myNumber = 13;
-console.log(myNumber);
+// Even or Odd Number
 
-myNumber = 14;
-console.log(myNumber);
+let remainder = n % 2;
 
+result = (0 == remainder) ? "even" : "odd";
+console.log("This Number is", result);
 
-// console
-let myName = "NurAlom";
-let myAge = 7;
-console.log('My Name Is', myName, 'and My age is', myAge, 'Years');
+// Negative or Positive Number
 
-
-// const
-const pi = 3.21; 
-// pi = 4.256; // const value con not be cheange
-console.log(pi);
-
-
-
-
-/* 
-JavaScript Reserved Words
-
-    They cannot be used as variable identifiers.
-
-await
-break
-case
-catch
-class
-const
-continue
-debugger
-default
-delete
-do
-else
-enum
-export
-extends
-false
-finally
-for
-function
-if
-implements
-import
-in
-instanceof
-interface
-let
-new
-null
-package
-private
-protected
-public
-return
-super
-switch
-static
-this
-throw
-try
-True
-typeof
-var
-void
-while
-with
-yield
-
-*/
+result = (n < 0) ? "Negative" : "Positive";
+console.log("This Number is", result);
