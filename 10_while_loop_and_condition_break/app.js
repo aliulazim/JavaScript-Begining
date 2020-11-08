6ae158af83f48e3381ec3b4f32d8abf6f85dba8b
aliulazim <aliulazimbd@gmail.com>
2020-11-08 08:51:27 +0600

    While Loop and Condition Break

---
+++ 10_while_loop_and_condition_break/app.js
@@ -0,0 +1,46 @@
+/*
+let i = 0;
+while (i < 15) {
+    console.log(i);
+    i++;
+    
+}
+
+*/
+
+
+// Even number
+
+let i = 0;
+while (i <= 16) {
+    if (i % 2 == 0) {
+        console.log(i); 
+    }
+    i++;
+    
+    
+}
+
+// Other ayea
+
+let n = 0;
+while (n <= 16) {
+    console.log(n);
+    n += 2;
+     
+}
+
+
+
+// Break Condition
+
+let x = 0;
+
+while (true) {
+    console.log(x);
+
+    if (x == 10) {
+        break;
+    }
+    x++;
+}
