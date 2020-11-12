5d14512199f0dc61ada8153465a19c202cb37201
aliulazim <aliulazimbd@gmail.com>
2020-11-12 17:20:43 +0600

    For loop Steping and break

---
+++ 13_for_loop_steping_and_break/app.js
@@ -0,0 +1,23 @@
+let i ;
+
+for (i = 0; i <= 10; i += 2) {
+    console.log(i);
+    
+    
+}
+
+console.log("\n Rest \n");
+
+
+i = 0;
+
+for (;;) {
+    i ++;
+    console.log(i);
+    if (10 == i) {
+        break;
+    }
+    
+    
+}
+
