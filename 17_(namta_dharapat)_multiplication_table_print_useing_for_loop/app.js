5b654ba16ee2cb80cb9569ff66a64df4148e4679
aliulazim <aliulazimbd@gmail.com>
2020-11-18 09:04:49 +0600

    (Namta Dharapat) Multiplicationn Table Print Useing For Loop

---
+++ 17_(namta_dharapat)_multiplication_table_print_useing_for_loop/app.js
@@ -0,0 +1,12 @@
+
+let series;
+ 
+for (let i = 1; i <= 10; i ++){
+    series = i + " = ";
+    for (let  j = 1;  j <= 10; j ++) {
+    //    series= series + i*j + " " ;
+       series += i*j + " ";
+        
+    }
+    console.log(series);
+}
