59fbdf451221198f10dc91190a51a385306029b7
aliulazim <aliulazimbd@gmail.com>
2020-11-21 09:15:37 +0600

    Create array and access element

---
+++ 20_create_array_and_access_element/app.js
@@ -0,0 +1,9 @@
+
+
+let fruits = ["apple", "banana", "orange", "plum", "grape"];
+
+console.log(fruits[4]);
+console.log(fruits.length);
+
+let lastOffset = fruits.length-2;
+console.log(fruits[lastOffset]);
