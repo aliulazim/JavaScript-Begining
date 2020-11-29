8c86e19c287349332150d67c618ed9bcc7f15afa
aliulazim <aliulazimbd@gmail.com>
2020-11-29 20:36:03 +0600

    Marge one array with another array

---
+++ 26_Merge one array with another array/app.js
@@ -0,0 +1,29 @@
+let list1 = [
+    "sun",  //0 = -7
+    "mon",  //1 = -6
+];
+
+
+let list2 = [
+
+    "tue",  //2 = -5
+    "wed",  //3 = -4
+
+];
+
+
+let list3 = [
+    "thu",  //4 = -3
+    "fri",  //5 = -2
+    "sat",  //6 = -1
+];
+
+
+let list = list1.concat(list2,list3);
+
+// Other weay
+// let list =[].concat(list1,list2,list3);
+
+
+
+console.log(list);
