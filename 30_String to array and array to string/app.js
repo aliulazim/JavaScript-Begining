09f4defa18214c6545b806536d43de341b353858
aliulazim <aliulazimbd@gmail.com>
2020-12-06 10:05:07 +0600

    String to array and array to string

---
+++ 30_String to array and array to string/app.js
@@ -0,0 +1,20 @@
+let  s = "Hi, I am aliul azim";
+let a = s.split(" ");
+
+console.log(a);
+
+let ns = a.join(" ");
+
+console.log(ns);
+
+console.log("orange.apple.banana.pineapple".split("."));
+console.log("orange.apple.banana.pineapple".split(".").join("-"));
+
+
+// Reverce word
+
+let name = "aliul azim";
+let rev = name.split("").reverse().join("");
+
+console.log(rev);
+
