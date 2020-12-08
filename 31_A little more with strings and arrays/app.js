6f5af081c40be3a898a0085581f62e9340d678c3
aliulazim <aliulazimbd@gmail.com>
2020-12-08 09:53:58 +0600

    a little more with strings and arrays

---
+++ 31_A little more with strings and arrays/app.js
@@ -0,0 +1,17 @@
+let s = "quick brown fox jumps over the lazy dog";
+let a = s.split(" ");
+
+//How many word
+console.log(a.length);
+
+// How many charecter
+
+let c = s.split("");
+
+console.log(c.length);
+
+// How many same later
+
+let u = s.split("u");
+
+console.log(u.length-1);
