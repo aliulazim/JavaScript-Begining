fcd0f63fd1398e0685945708f90b06dbd641626a
aliulazim <aliulazimbd@gmail.com>
2020-12-09 10:29:03 +0600

    The default value of a fanction parameter

---
+++ 33_The default value of a function parameter/app.js
@@ -0,0 +1,11 @@
+
+
+function sayHi(name = "anonymous", age = "unkhown") {
+    console.log("Hello "+name+", You are "+age+" years old");
+    
+}
+
+sayHi("Rahim", 24);
+sayHi("Kahim", 21);
+sayHi();
+sayHi();
